6af00e55fb744b7f4d6ed3a0f7dd179708af5bdb
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-02-05 23:05:51 +0530

    Comment added

--- total_persetage.c
+++ total_persetage.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main()
-{
+{  
+    // This program calculates the total marks and percentage of five subjects.
+
     float a, b, c, d, e, total, percentage;
     printf("Enter marks of five subjects: ");
     scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
@@ -9,4 +11,5 @@ int main()
     percentage = (total / 500) * 100;
     printf("Percentage: %.2f\n", percentage);
     return 0;
+    
 }
