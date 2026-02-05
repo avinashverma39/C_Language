0b696e2d60ebaf553faca1b03ac14bc98e6b5fe3
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-02-05 22:59:58 +0530

    Total and persentage of 5 numbers

---
+++ total_persetage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float a, b, c, d, e, total, percentage;
+    printf("Enter marks of five subjects: ");
+    scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
+    total = a + b + c + d + e;
+    printf("Total marks: %.2f\n", total);
+    percentage = (total / 500) * 100;
+    printf("Percentage: %.2f\n", percentage);
+    return 0;
+}
