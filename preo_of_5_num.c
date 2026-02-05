6251c1e0664c24a05a532bcf1e357231d0689be6
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-02-05 22:49:55 +0530

    presentage of three number

---
+++ preo_of_5_num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float num1, num2, num3, average;
+    printf("Enter three numbers: ");
+    scanf("%f %f %f", &num1, &num2, &num3);
+    average = (num1 + num2 + num3) / 3;
+    printf("The average of %.2f, %.2f and %.2f is %.2f", num1, num2, num3, average);
+    return 0;
+}
