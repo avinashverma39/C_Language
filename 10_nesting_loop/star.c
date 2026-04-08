da8b9c7dcea6f46d4c4bef7841f152da0899de1a
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-04-08 22:00:28 +0530

    Add some new code

--- 10_nesting_loop/star.c
+++ 10_nesting_loop/star.c
@@ -2,12 +2,16 @@
 // *
 // **
 // ***
-
+// using nested loops
+// For n trms
 #include <stdio.h>
 int main()
 {
-    int i, j;
-    for (i = 1; i <= 3; i++)
+    int i, j, n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
     {
         for (j = 1; j <= i; j++)
         {
